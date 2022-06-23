6cf83229bf1a0fbb41bac0d25e59effe962fe548
anjun65 <hajrulkhaira@gmail.com>
2022-06-23 11:19:02 +0700

    test

--- resources/views/components/important-date.blade.php
+++ resources/views/components/important-date.blade.php
@@ -19,6 +19,9 @@
                         <li>a PDF copy of your paper formatted according to the above templates, and</li>
                         <li>an archive file (e.g. zip, tar.gz) containing the both a PDF copy of your paper and LaTeX or Word source material prepared according to the above guidelines.</li>
                     </ol>
+
+                    <p class="py-4 text-base text-gray-800"><b>ONLINE SUBMISSION</b> via <b><a href="https://edas.info/N29938" target="_blank" >EDAS</a> </b></p>
+                    <a href="https://edas.info/N29938" target="_blank" ><img src="img/edas.png" alt="" class="h-16 w-auto mb-4"></a>
                 </p>
 
             </div>
