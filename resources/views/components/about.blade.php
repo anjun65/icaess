a3f5ba6813567256daebecbded96ee2628752585
anjun65 <hajrulkhaira@gmail.com>
2022-06-23 14:50:35 +0700

    update icaess

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -70,8 +70,8 @@
     <section class="bg-white">
         <div class="max-w-5xl px-6 py-16 mx-auto">
             <h2 class="text-3xl font-semibold text-gray-800 text-justify">About the Conference</h2>
-            <p class="mt-4 text-gray-600 text-justify">Who can deny the fact that the Metaverse is taking the world by storm? Ready or not, the metaverse will change the world right now, for the next decade. Every day, the Metaverse opens up new opportunities for collaboration and interaction.
-            With the development of deep learning technology, natural generation models, VR/AR/MR/XR technology, along with 5G and WiFi6 communication technologies, Metaverse is strengthened by a variety of factors, from mobile-based always connected, to virtual currency for meta-economics. Without the limitations of space-time, the Metaverse offers us endless possibilities.
+            <p class="mt-4 text-lg text-gray-600 text-justify">Who can deny the fact that the Metaverse is taking the world by storm? Ready or not, the metaverse will change the world right now, for the next decade. Every day, the Metaverse opens up new opportunities for collaboration and interaction.
+            With the development of deep learning technology, natural generation models, VR/AR/MR/XR technology, along with 5G and WiFi6 communication technologies, Metaverse is strengthened by a variety of factors, from mobile-based always connected, to virtual currency for meta-economics. <br/><br/> Without the limitations of space-time, the Metaverse offers us endless possibilities.
             There are also many opportunities to develop technologies that supports metaverse, like the electronic components, the mechanical system, the applications program, and the implementation in business. We invite you to ICAE/ICAESS 2022 to discuss about this very opportunities. We look forward to meeting you at ICAE/ICAESS 2022!</p>
             <div class="grid gap-8 mt-10 md:mt-20 md:grid-cols-2">
                 <div class="card w-full pl-2 shadow-xl">
