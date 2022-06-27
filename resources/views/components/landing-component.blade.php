967a584b834f1d8af7c0d0a2f404ce49bb553a7e
anjun65 <hajrulkhaira@gmail.com>
2022-06-27 16:00:09 +0700

    update

--- resources/views/components/landing-component.blade.php
+++ resources/views/components/landing-component.blade.php
@@ -141,7 +141,7 @@
             for (var i = 0; i < 2; i++) {
               zero += "0";
             }
-            return (zero + num).slice(-3);
+            return (zero + num).slice(-2);
           },
         };
       }
