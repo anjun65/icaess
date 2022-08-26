cc9102b911620ca21128f857aa24b4f69de565ea
anjun65 <hajrulkhaira@gmail.com>
2022-08-26 09:48:11 +0700

    ayo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,6 +23,12 @@
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
+
+        <div style="border-radius: 50%;bottom: 0px;color: #979797;cursor: pointer;height: auto;position: fixed;text-align: center;width: 4rem;right:0px;z-index: 9;display: block;padding:0px 0px 5px 0">
+          <a href="https://wa.me/6283821557379">
+            <img src="img/whatsapp.png" alt="">
+          </a>
+        </div>
     </body>
 
     <x-footer></x-footer>
