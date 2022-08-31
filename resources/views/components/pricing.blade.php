75c1148afb92ce7676b5d88777d6fb7cc3779d23
anjun65 <hajrulkhaira@gmail.com>
2022-08-31 14:59:42 +0700

    ayo

--- resources/views/components/pricing.blade.php
+++ resources/views/components/pricing.blade.php
@@ -1,11 +1,11 @@
 <div>
 <div class="bg-white text-center">
   <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 m-auto">
-    <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">Registration Fee</h2>
+    <h2 class="mb-5 text-2xl font-extrabold tracking-tight text-gray-900">Registration Fee</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       
       <div>
-        <div class="card w-full bg-base-100 shadow-xl h-full">
+        <div class="card w-full bg-white shadow-xl h-full">
           <div class="card-body">
             <h2 class="font-bold text-center">EARLY BIRD</h2>
             <b>For The Fast One</b>
@@ -29,7 +29,7 @@
       
       
       <div>
-        <div class="card w-full bg-base-100 shadow-xl h-full">
+        <div class="card w-full bg-white shadow-xl h-full">
           <div class="card-body">
             <h2 class="font-bold text-center">Late Registration</h2>
             <br/>
@@ -52,7 +52,7 @@
       </div>
 
       <div>
-        <div class="card w-full bg-base-100 shadow-xl h-full">
+        <div class="card w-full bg-white shadow-xl h-full">
           <div class="card-body">
             <h2 class="font-bold text-center">MISCELLANEOUS</h2>
             <p>
