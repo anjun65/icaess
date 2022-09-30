005ae23176d4a84309b611dad623f14f107f8804
anjun65 <hajrulkhaira@gmail.com>
2022-09-30 08:45:52 +0700

    ayo

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,9 +9,9 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
             @livewire('admin.paper')
 
-            @livewire('admin.poster')
+            {{-- @livewire('admin.poster')
 
-            @livewire('admin.payment-proof')
+            @livewire('admin.payment-proof') --}}
         </div>
     </main>
 
