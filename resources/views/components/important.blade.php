7305fbf3280893f848feb51e89bace17b5da87a4
anjun65 <hajrulkhaira@gmail.com>
2023-08-10 12:27:48 +0700

    test

---
+++ resources/views/components/important.blade.php
@@ -0,0 +1,81 @@
+
+<div class="py-10 relative bg-white overflow-hidden">
+  <div class="max-w-7xl mx-auto">
+    <section class="bg-white">
+        <div class="max-w-5xl px-6 py-16 mx-auto">
+            <div class="p-4 mb-4 rounded-lg shadow-sm  xl:mb-0">
+              <div class="flex items-center justify-between mb-4">
+                <h3 class="text-3xl font-semibold text-gray-800 uppercase text-justify">Important Dates</h3>
+              </div>
+              
+              <div class="card shadow-xl w-full lg:w-1/2">
+                <table class="w-full py-5">
+                <thead>
+                    <tr tabindex="0" class="focus:outline-none h-16 w-full text-sm leading-none bg-gray-50  text-gray-800">
+                        <th class="font-normal text-left pl-4">Event</th>
+                        <th class="font-normal text-left p-6">Date</th>
+                    </tr>
+                </thead>
+                <tbody class="w-full">
+                    <tr tabindex="0" class="focus:outline-none h-20 text-sm leading-none text-gray-800 hover:bg-gray-100 border-b border-t border-gray-100">
+                        <td class="pl-4 cursor-pointer">
+                            <div class="flex items-center">
+                                <div>
+                                    <p class="font-medium">Final Full Paper Submission</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="p-6">
+                            <p class="text-sm font-medium leading-none text-gray-800">18 September 2022</p>
+                        </td>
+                    </tr>
+
+                    <tr tabindex="0" class="focus:outline-none h-20 text-sm leading-none text-gray-800 hover:bg-gray-100 border-b border-t border-gray-100">
+                        <td class="pl-4 cursor-pointer">
+                            <div class="flex items-center">
+                                <div>
+                                    <p class="font-medium">Registration and Payment</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="p-6">
+                            <p class="text-sm font-medium leading-none text-gray-800">22 September 2022</p>
+                        </td>
+                    </tr>
+
+                    <tr tabindex="0" class="focus:outline-none h-20 text-sm leading-none text-gray-800 hover:bg-gray-100 border-b border-t border-gray-100">
+                        <td class="pl-4 cursor-pointer">
+                            <div class="flex items-center">
+                                <div>
+                                    <p class="font-medium">Camera Ready and Final Revision</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="p-6">
+                            <p class="text-sm font-medium leading-none text-gray-800">06 October 2023</p>
+                        </td>
+                    </tr>
+
+
+                    <tr tabindex="0" class="focus:outline-none h-20 text-sm leading-none text-gray-800 hover:bg-gray-100 border-b border-t border-gray-100">
+                        <td class="pl-4 cursor-pointer">
+                            <div class="flex items-center">
+                                <div>
+                                    <p class="font-medium">Conference Day</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="p-6">
+                            <p class="text-sm font-medium leading-none text-gray-800">07-08 November 2023</p>
+                        </td>
+                    </tr>
+
+                    
+                </tbody>
+            </table>
+            </div>
+            </div>
+        </div>
+    </section>
+  </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                  <x-about2023/>
+                 <x-important/>
             </div>
         </div>
     </div>
