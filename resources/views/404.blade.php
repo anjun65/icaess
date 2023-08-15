606d879f9875a886cc328ca892be1f28193bb928
anjun65 <hajrulkhaira@gmail.com>
2023-08-15 12:15:29 +0700

    test

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -5,7 +5,7 @@
                 <div class="flex flex-col items-center py-16">
                     <h1 class="px-4 pt-8 pb-4 text-center text-5xl font-bold leading-10 text-gray-800">Coming soon</h1>
                     
-                    <a href="{{ route('home') }}" class="mt-5 btn text-center mx-4 h-10 w-44 rounded-md text-white text-base bg-indigo-700 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-opacity-50 focus:ring-indigo-800">Go Back</a>
+                    <a href="{{ route('home-2023') }}" class="mt-5 btn text-center mx-4 h-10 w-44 rounded-md text-white text-base bg-indigo-700 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-opacity-50 focus:ring-indigo-800">Go Back</a>
                 </div>
             </div>
         </div>
