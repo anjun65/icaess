317322d4f37cefce1c3232d80e8416701ab976a2
anjun65 <hajrulkhaira@gmail.com>
2023-08-18 11:53:49 +0700

    test

--- resources/views/components/footer2023.blade.php
+++ resources/views/components/footer2023.blade.php
@@ -1,12 +1,13 @@
 <div>
     <footer class="mx-auto w-full max-w-container px-4 sm:px-6 lg:px-8">
         <div class="border-t border-slate-900/5 py-10">
-                <p class="mx-auto h-5 w-auto text-slate-900">
+                <p class="mx-auto h-5 w-auto">
                     <img src="{{ asset('img/logo-wh.png') }}" alt="" class="h-8 mx-auto">
                 <p>
                 <p class="mt-5 text-center text-sm leading-6 text-slate-500">
-                    © 2023 Polibatam Cyber Team. All rights reserved.<br/>
-                    Lantai 11, Tower Mohammad Nasir, Politeknik Negeri Batam
+                    © 2023 Politeknik Negeri Batam. All rights reserved.<br/>
+                    Batam Centre, Jl. Ahmad Yani, Tlk. Tering, Kec. Batam Kota, Kota Batam, Kepulauan Riau 29461
+                    <br/> Email : icaess@polibatam.ac.id
                 </p>
         </div>
     </footer>
