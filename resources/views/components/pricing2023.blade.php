a3d401478e73df2fe99af37c490cf905c64e97fc
anjun65 <hajrulkhaira@gmail.com>
2023-08-28 16:32:19 +0700

    test

--- resources/views/components/pricing2023.blade.php
+++ resources/views/components/pricing2023.blade.php
@@ -2,9 +2,35 @@
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-md sm:text-center">
           <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl ">Register Now </h2>
+          <p class="mx-auto mb-8 max-w-2xl font-light text-gray-800 md:mb-12 sm:text-xl ">Contact : icae@polibatam.ac.id</p>
           
-          <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl "> Register Now for Rp. 2.750.000<br/>Contact : icaess@polibatam.ac.id</p>
-          
+          <dl class="grid max-w-screen-xl grid-cols-1 gap-8 p-4 mx-auto text-gray-900 md:grid-cols-3 sm:p-8">
+                <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">USD 200</dt>
+                    <dd class="text-gray-500">for International Regular</dd>
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">Rp 2.750.000</dt>
+                    <dd class="text-gray-500">for Indonesia Regular</dd>
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">Free</dt>
+                    <dd class="text-gray-500">for Poster Regular</dd>
+                </div>
+          </dl>
+
+          <dl class="grid max-w-screen-xl grid-cols-1 gap-8 p-4 mx-auto text-gray-900 md:grid-cols-2 sm:p-8">
+                
+                <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">Rp 500.000</dt>
+                    <dd class="text-gray-500">for Student</dd>
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">Free</dt>
+                    <dd class="text-gray-500">for Participant</dd>
+                </div>
+          </dl>
+
       </div>
   </div>
 </section>
