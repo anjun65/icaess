2ab99047991dd7df569f6e5439892048aa1ebecf
anjun65 <hajrulkhaira@gmail.com>
2023-09-04 15:21:23 +0700

    test

--- resources/views/components/speaker-2023.blade.php
+++ resources/views/components/speaker-2023.blade.php
@@ -1,23 +1,23 @@
 <section class="bg-white ">
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-md sm:text-center">
-          <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl ">Speaker </h2>
+          <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl ">Keynote Speakers</h2>
 
           <div class="grid max-w-screen-xl grid-cols-1 gap-8 p-4 mx-auto text-gray-900 md:grid-cols-2 sm:p-8">
                 
                 <div class="flex flex-col items-center justify-center ">
                   <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
-                    <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="{{ asset('img/speaker/Dr.Ir. Basuki Rahmatul Alam.jpg') }}" alt="Speaker">
-                    <h1 class="text-lg text-gray-700">Dr.Ir. Basuki Rahmatul Alam</h1>
-                    <h3 class="text-sm text-gray-400 ">Retired ITB lecturer/ founder TFME/ Polibatam lecturer / Senior Member IEEE</h3>
+                    <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="{{ asset('img/speaker/Prof. Dr. Benny Tjahjono.jpg') }}" alt="Speaker">
+                    <h1 class="text-lg text-gray-700">Prof. Dr. Benny Tjahjono</h1><br/>
+                    <h3 class="text-sm text-gray-400 ">Professor of Sustainability and Supply Chain Management, Research Centre for Business in Society</h3><br/>
 
                   </div>
                 </div>
                 <div class="flex flex-col items-center justify-center ">
                   <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
-                    <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="{{ asset('img/speaker/Dr MK Radhakrishnan.jpg') }}" alt="Speaker">
-                    <h1 class="text-lg text-gray-700">Dr MK Radhakrishnan</h1>
-                    <h3 class="text-sm text-gray-400 ">NanoRel - LLP Technical Consultant, Spore / VP IEEE EDS</h3>
+                    <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="{{ asset('img/speaker/TS Dr Nurulizwa Binti Abdul Rashid.jpg') }}" alt="Speaker">
+                    <h1 class="text-lg text-gray-700">TS Dr Nurulizwa Binti Abdul Rashid</h1>
+                    <h3 class="text-sm text-gray-400 ">Deputy Dean of Research and Postgraduate Studies at the Faculty of Technology Management and Technopreneurship, UTeM.</h3>
 
                   </div>
                 </div>
